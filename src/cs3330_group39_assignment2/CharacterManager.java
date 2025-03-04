9298eae4a199030b5deb89f90ae83db8cc344514
qmcco <qmccollister03@gmail.com>
2025-03-04 13:36:32 -0600

    Added initial versions of CharacterManager and MiddleEarthCouncil class
    Added CharacterManager and MiddleEarthCouncil classes, both are
    (relatively) empty.

---
+++ src/cs3330_group39_assignment2/CharacterManager.java
@@ -0,0 +1,7 @@
+package cs3330_group39_assignment2;
+
+public class CharacterManager {
+	private MiddleEarthCharacter[] characters;
+	private int size;
+	
+}
---
+++ src/cs3330_group39_assignment2/MiddleEarthCouncil.java
@@ -0,0 +1,14 @@
+package cs3330_group39_assignment2;
+
+class MiddleEarthCouncil {
+	private static MiddleEarthCouncil instance;
+	public static MiddleEarthCouncil getInstance() {
+		if(instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		return instance;
+	}
+	public CharacterManager getCharacterManager() {
+		
+	}
+}
